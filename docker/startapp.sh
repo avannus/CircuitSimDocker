043a9d0c7e71badc3345a14455dd3971d1fccf96
Andrew <avannus@gmail.com>
2023-01-19 13:16:57 -0500

    moved to alpine, tested

--- docker/startapp.sh
+++ docker/startapp.sh
@@ -3,4 +3,8 @@
 
 export HOME=/config
 
-exec java --module-path /usr/share/openjfx/lib/ --add-modules=javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web -jar /CircuitSim
+exec java \
+--module-path /usr/lib/openjfx/ \
+--add-modules=javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web \
+-jar \
+/CircuitSim
